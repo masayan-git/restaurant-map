723f73f9031ae949bb899f79c56582e6238b5adf
masayan-git <68589347+masayan-git@users.noreply.github.com>
2023-07-11 16:08:25 +0900

    MUIのリセットCSS CssBaseline実装

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,12 @@
-import '@/styles/globals.scss'
-import type { AppProps } from 'next/app'
+import { CssBaseline } from "@mui/material";
+import "@/styles/globals.scss";
+import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </>
+  );
 }
